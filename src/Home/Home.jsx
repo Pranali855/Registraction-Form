5ac55cc9dfb6c086427c22019b17b5a668e62273
pranali <kokatepranu833@gmail.com>
2026-04-05 09:01:18 +0530

    Update Home.jsx
    basic changes added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,209 +1,209 @@
 import React, { useEffect } from 'react'
 import './Home.css'
-import {useState} from 'react'
+import { useState } from 'react'
 
 function Home() {
- const[form,setForm]=useState({
-    name:'',
-    age:'',
-    city:'',
-    error:'',
-    gender:'',
-    subject:[]
- });
+    const [form, setForm] = useState({
+        name: '',
+        age: '',
+        city: '',
+        error: '',
+        gender: '',
+        subject: []
+    });
 
- useEffect(()=>{
-   if (!!form.name && !!form.age) {
-    return;
-} else if (form.name.length > 0 && form.name.length < 3) {
-    setForm({ ...form, error: "Name must be at least 3 characters long" });
-} else if (form.name.length > 20) {
-    setForm({ ...form, error: "Name must be less than 20 characters long" });
-} else if (form.age < 20) {
-    setForm({ ...form, error: "Age must be at least 20 years old" });
-} else if (form.age > 60) {
-    setForm({ ...form, error: "Age must be less than 60 years old" });
-} else {
-    setForm({ ...form, error: "" });
-}
-
- },[form.name,form.age,form.error])
-
-
- useEffect(()=>{
-    const storedData=localStorage.getItem("userData");
-    if(storedData){
-        const parsedData=JSON.parse(storedData);
-
-        if(parsedData.name===form.name){
-            return
+    useEffect(() => {
+        if (!!form.name && !!form.age) {
+            return;
+        } else if (form.name.length > 0 && form.name.length < 3) {
+            setForm({ ...form, error: "Name must be at least 3 characters long" });
+        } else if (form.name.length > 20) {
+            setForm({ ...form, error: "Name must be less than 20 characters long" });
+        } else if (form.age < 20) {
+            setForm({ ...form, error: "Age must be at least 20 years old" });
+        } else if (form.age > 60) {
+            setForm({ ...form, error: "Age must be less than 60 years old" });
+        } else {
+            setForm({ ...form, error: "" });
         }
-        setForm({
-            ...form,
-            name:parsedData.name || "",
-            age:parsedData.age ||"",
-            error:"",
-        })
-    }
- },[])
- const saveName=()=>{
-    if(form.error){
-        alert(form.error);
-        return;
-    }
-   
-    localStorage.setItem("userData",JSON.stringify(form))
 
- }
-  return (
-    <div className='form'>
-        <h1>Ragistration Form </h1>
-        <p>Hello {form.name || "User"}❤️ You are {form.age || "User"} years old.</p>
-
-        <div className="1">
-        <input type='text' placeholder='Enter Your Name' className='input'
-        onChange={(e)=>{
-            console.log("chenged happpen",e.target.value);
-            setForm({...form,name:e.target.value})
-        }} value={form.name}/>
-    </div>
-
-    <div>
-        <select onChange={(e)=>{
-            setForm({...form,city:e.target.value})
-        }} value={form.city} className='input'>
-            <option>Select your city</option>
-    <option value="delhi">Delhi</option>
-    <option value="bangalore">Bangalore</option>
-    <option value="hyderabad">Hyderabad</option>
-    <option value="kolkata">Kolkata</option>
-    <option value="jaipur">Jaipur</option>
-        </select>
-    </div>
-
-    <div>
-        <p>Select Gender</p>
-        <input type='radio' name='gender' value="female" checked={form.gender==="female"}
-        onChange={(e)=>{
-            setForm({...form,gender:e.target.value})
-        }}
-        />Female
-        <input type='radio' name='gender' value="male"checked={form.gender==="male"}
-        onChange={(e)=>{
-        setForm({...form,gender:e.target.value})
-        }}
-        />male
-    </div>
+    }, [form.name, form.age, form.error])
 
 
-    <div>
-       <p> Choose your favourite Subjects</p>
-       <input type='checkbox' value="math" checked={form.subject.includes("math")}
-       onChange={(e)=>{
-        const val=e.target.value;
-        if(form.subject.includes(val)){
+    useEffect(() => {
+        const storedData = localStorage.getItem("userData");
+        if (storedData) {
+            const parsedData = JSON.parse(storedData);
+
+            if (parsedData.name === form.name) {
+                return
+            }
             setForm({
                 ...form,
-                subject:form.subject.filter(
-                    (subject)=>subject!==val
-                )
+                name: parsedData.name || "",
+                age: parsedData.age || "",
+                error: "",
             })
-        }else{
-         setForm({
-                ...form,
-            subject:[...form.subject,val]
-         })
         }
-       }}
-       />{" "} Math
-       <input type='checkbox' value="marathi" checked={form.subject.includes("marathi")}
-              onChange={(e)=>{
-        const val=e.target.value;
-        if(form.subject.includes(val)){
-            setForm({
-                ...form,
-                subject:form.subject.filter(
-                    (subject)=>subject!==val
-                )
-            })
-        }else{
-         setForm({
-                ...form,
-            subject:[...form.subject,val]
-         })
+    }, [])
+    const saveName = () => {
+        if (form.error) {
+            alert(form.error);
+            return;
         }
-       }}
 
-       />hindi
-       <input 
-  type='checkbox' 
-  value="science" 
-  checked={form.subject.includes("science")}
-  onChange={(e)=>{
-    const val=e.target.value;
-    if(form.subject.includes(val)){
-        setForm({
-            ...form,
-            subject:form.subject.filter(
-                (subject)=>subject!==val
-            )
-        })
-    }else{
-     setForm({
-            ...form,
-        subject:[...form.subject,val]
-     })
+        localStorage.setItem("userData", JSON.stringify(form))
+
     }
-  }}
-/> Science
+    return (
+        <div className='form'>
+            <h1>Ragistration Form </h1>
+            <p>Hello {form.name || "User"}❤️ You are {form.age || "User"} years old.</p>
 
-       <input type='checkbox' value="hindi" checked={form.subject.includes("hindi")}
-              onChange={(e)=>{
-        const val=e.target.value;
-        if(form.subject.includes(val)){
-            setForm({
-                ...form,
-                subject:form.subject.filter(
-                    (subject)=>subject!==val
-                )
-            })
-        }else{
-         setForm({
-                ...form,
-            subject:[...form.subject,val]
-         })
-        }   
-       }}
-/>hindi
+            <div className="1">
+                <input type='text' placeholder='Enter Your Name' className='input'
+                    onChange={(e) => {
+                        console.log("chenged happpen", e.target.value);
+                        setForm({ ...form, name: e.target.value })
+                    }} value={form.name} />
+            </div>
 
-    </div>
+            <div>
+                <select onChange={(e) => {
+                    setForm({ ...form, city: e.target.value })
+                }} value={form.city} className='input'>
+                    <option>Select your city</option>
+                    <option value="delhi">Delhi</option>
+                    <option value="bangalore">Bangalore</option>
+                    <option value="hyderabad">Hyderabad</option>
+                    <option value="kolkata">Kolkata</option>
+                    <option value="jaipur">Jaipur</option>
+                </select>
+            </div>
 
-        
-        <div className="2">
-        <input type="number" placeholder='Enter your age' className='input' value={form.age} onChange={(e)=>{
-            setForm({...form,age:e.target.value})
-        }}/>
-        </div> 
+            <div>
+                <p>Select Gender</p>
+                <input type='radio' name='gender' value="female" checked={form.gender === "female"}
+                    onChange={(e) => {
+                        setForm({ ...form, gender: e.target.value })
+                    }}
+                />Female
+                <input type='radio' name='gender' value="male" checked={form.gender === "male"}
+                    onChange={(e) => {
+                        setForm({ ...form, gender: e.target.value })
+                    }}
+                />male
+            </div>
 
-        <p className='error-text'>{form.error}</p>
-        
-        <div className="Button">
-        <button className={`btn ${form.error?'btn-disabled':null}`} onClick={saveName}>Save</button>
-        <button className='btn' onClick={()=>{
-            setForm({
-                name:"",
-                age:"",
-                error:"",
-                city:"",
-                gender:"",
-                subject:[]
-            })
-             localStorage.clear()
-        }}>Clear</button> 
-        
+
+            <div>
+                <p> Choose your favourite Subjects</p>
+                <input type='checkbox' value="math" checked={form.subject.includes("math")}
+                    onChange={(e) => {
+                        const val = e.target.value;
+                        if (form.subject.includes(val)) {
+                            setForm({
+                                ...form,
+                                subject: form.subject.filter(
+                                    (subject) => subject !== val
+                                )
+                            })
+                        } else {
+                            setForm({
+                                ...form,
+                                subject: [...form.subject, val]
+                            })
+                        }
+                    }}
+                />{" "} Math
+                <input type='checkbox' value="marathi" checked={form.subject.includes("marathi")}
+                    onChange={(e) => {
+                        const val = e.target.value;
+                        if (form.subject.includes(val)) {
+                            setForm({
+                                ...form,
+                                subject: form.subject.filter(
+                                    (subject) => subject !== val
+                                )
+                            })
+                        } else {
+                            setForm({
+                                ...form,
+                                subject: [...form.subject, val]
+                            })
+                        }
+                    }}
+
+                />hindi
+                <input
+                    type='checkbox'
+                    value="science"
+                    checked={form.subject.includes("science")}
+                    onChange={(e) => {
+                        const val = e.target.value;
+                        if (form.subject.includes(val)) {
+                            setForm({
+                                ...form,
+                                subject: form.subject.filter(
+                                    (subject) => subject !== val
+                                )
+                            })
+                        } else {
+                            setForm({
+                                ...form,
+                                subject: [...form.subject, val]
+                            })
+                        }
+                    }}
+                /> Science
+
+                <input type='checkbox' value="hindi" checked={form.subject.includes("hindi")}
+                    onChange={(e) => {
+                        const val = e.target.value;
+                        if (form.subject.includes(val)) {
+                            setForm({
+                                ...form,
+                                subject: form.subject.filter(
+                                    (subject) => subject !== val
+                                )
+                            })
+                        } else {
+                            setForm({
+                                ...form,
+                                subject: [...form.subject, val]
+                            })
+                        }
+                    }}
+                />hindi
+
+            </div>
+
+
+            <div className="2">
+                <input type="number" placeholder='Enter your age' className='input' value={form.age} onChange={(e) => {
+                    setForm({ ...form, age: e.target.value })
+                }} />
+            </div>
+
+            <p className='error-text'>{form.error}</p>
+
+            <div className="Button">
+                <button className={`btn ${form.error ? 'btn-disabled' : null}`} onClick={saveName}>Save</button>
+                <button className='btn' onClick={() => {
+                    setForm({
+                        name: "",
+                        age: "",
+                        error: "",
+                        city: "",
+                        gender: "",
+                        subject: []
+                    })
+                    localStorage.clear()
+                }}>Clear</button>
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Home;
